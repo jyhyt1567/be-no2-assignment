71c422a96fe1727fc00d953b48508471b7e3a3d8
jyhyt1567 <jyhyt1567@gmail.com>
2025-05-20 00:00:06 +0900

    init commit

---
+++ src/main/java/org/example/beno2assignment/BeNo2AssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.beno2assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeNo2AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeNo2AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/beno2assignment/BeNo2AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.beno2assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeNo2AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
